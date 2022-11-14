8f55d607137c1ea2b9bc260819ff8239fec50768
BarrioTropical <barriotropical@gmail.com>
2022-11-14 14:58:35 +0100

    sass creation, import variables color,  components
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '../assets/styles/_partials/variables.scss' as *;
+    @use '../assets/styles/general.scss' as *;
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+Main
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '../assets/styles/_partials/variables.scss' as *;
+    @use '../assets/styles/general.scss' as *;
+</style>

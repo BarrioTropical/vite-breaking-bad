32ed7abb665f02501298dd959bcf475ad12c3dca
BarrioTropical <barriotropical@gmail.com>
2022-11-15 17:08:05 +0100

    store js
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiURL: 'https://www.breakingbadapi.com/api/',
+    characterList: [],
+    loading: false,
+    search: {
+        status: '',
+        name: ''
+    },
+    errormessage: ''
+});
